c6f6b563dce0582b8307a694c4bfa11f14cd602e
Vanh Nguyễn <VietAnhScout@gmail.com>
2023-09-10 16:56:02 +0700

    delete history

--- .history/src/navigations/config_20230907163146.ts
+++
@@ -1,8 +0,0 @@
-import { NavigatorScreenParams } from "@react-navigation/native";
-
-export type RootStackParams = {
-  Home: undefined;
-  Search: undefined;
-  Favourite: undefined;
-  Profile: undefined;
-};
