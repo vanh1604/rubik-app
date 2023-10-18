851539b4fbd08bc2585bda84fb9782c9ac45f03f
Vanh Nguyễn <VietAnhScout@gmail.com>
2023-10-18 20:13:39 +0700

    fix

--- src/constansts/leagueInfor.tsx
+++ src/constansts/leagueInfor.tsx
@@ -1,28 +1,30 @@
 import { LeagueItemProps } from "../components/league/LeagueItem";
 import { Calendar } from "iconsax-react-native";
 
-export const ContentProps = [{
-  id: 1,
-  title: "Giải Rubik WCA “Hanoi Super Brain Zyo 2029",
-  time: "Giải đấu sẽ được diễn ra trong 2 ngày: Ngày 25/02/2023: Bắt đầu thi đấu từ: 08h00 sáng – Kết thúc lúc 18h00 chiều. Ngày 26/02/2023: Bắt đầu thi đấu từ: 08h00 sáng – Kết thúc lúc 18h00 chiều.",
-  location:
-    "Giải đấu được tổ chức tại : The Sporthall, Newton Grammar School – Trường Quốc tế THCS – THPT Newton Địa chỉ: 136 Hồ Tùng Mậu, Bắc Từ Liêm District, Hà Nội. Click để xem trên Google Map",
-  signin:
-    "Đơn đăng ký trực tuyến sẽ được mở từ 14h00 Thứ Năm, 12 tháng 01, 2023, và kết thúc vào lúc 07h00 Thứ Năm, 23 tháng 02, 2023.",
-  req: "Đơn đăng ký trực tuyến sẽ được mở từ 14h00 Thứ Năm, 12 tháng 01, 2023, và kết thúc vào lúc 07h00 Thứ Năm, 23 tháng 02, 2023.",
-  fees: "* Phí cơ bản cho 1 nội dung đầu tiên: 100.000₫ (Vietnamese Đồng).* Từ 2 nội dung trở lên: 30.000₫/nội dung* Phí cho 5 nội dung : 200.000₫ (Vietnamese Đồng).* Phí cho 10 nội dung : 300.000₫ (Vietnamese Đồng).* Phí cho 14 nội dung: 350.000₫ (Vietnamese Đồng).Đăng ký tại bàn ở giải đấu sẽ được chấp nhận với mức lệ phí nhân đôi so với đăng ký trên web.* Sau khi thanh toán lệ phí thi, đơn đăng ký của bạn sẽ được chấp nhận trong 24h Bạn có thể thanh toán lệ phí thi bằng cách chuyển khoản đến tài khoản dưới đây:Số tài khoản: (MB Bank – Ngân hàng Quân Đội) 0987896610Tên Tài Khoản: NGUYEN DUC VINH Sau khi chuyển khoản vui lòng gửi ảnh chứng minh giao dịch thành công cùng với tên của bạn hoặc ID WCA của bạn tới email: vncubeassociation@gmail.com Sau khi đăng ký và nộp lệ phí thi. Đăng ký của bạn sẽ được xác nhận trong vòng 24 giờ.Nếu đăng ký của bạn không được xác nhận trong vòng 24 giờ xin vui lòng liên hệ với chúng tôi. Nếu bạn không thể trả phí theo phương pháp trên, vui lòng liên hệ với chúng tôi. Lưu ý: tên đăng ký phải viết tiếng Việt có dấu, đúng thứ tự họ – tên đệm – tên, nếu không sẽ không được chấp nhận. Thí sinh có thể liên hệ ban tổ chức để thay đổi bộ môn thi, hạn cuối là ngày 25-02-2023. Tất cả thí sinh trong danh sách chờ sẽ bị loại bỏ và hoàn tiền (nếu đã đóng) sau ngày 22-02-2023. Tất cả lệ phí thi PHẢI ĐƯỢC chuyển khoản trước Sau khi đăng ký và chuyển khoản lệ phí, thời gian xác nhận đăng ký là 2-3 ngày làm việc. Nếu có thắc mắc, các bạn có thể email cho BTC. Với những bạn đã có vé tham dự WCA của Zyo Rubik (nhà tài trợ giải đấu), vui lòng mang theo vé và sẽ nhận lại tiền tại bàn check in thi đấu. Chi tiết liên hệ: ZyoRubik",
-},{
-  id: 2,
-   title: "Giải Rubik WCA “Hanoi Super Brain Zyo 2025",
-  time: "Giải đấu sẽ được diễn ra trong 2 ngày: Ngày 25/02/2023: Bắt đầu thi đấu từ: 08h00 sáng – Kết thúc lúc 18h00 chiều. Ngày 26/02/2023: Bắt đầu thi đấu từ: 08h00 sáng – Kết thúc lúc 18h00 chiều.",
-  location:
-    "Giải đấu được tổ chức tại : The Sporthall, Newton Grammar School – Trường Quốc tế THCS – THPT Newton Địa chỉ: 136 Hồ Tùng Mậu, Bắc Từ Liêm District, Hà Nội. Click để xem trên Google Map",
-  signin:
-    "Đơn đăng ký trực tuyến sẽ được mở từ 14h00 Thứ Năm, 12 tháng 01, 2023, và kết thúc vào lúc 07h00 Thứ Năm, 23 tháng 02, 2023.",
-  req: "Đơn đăng ký trực tuyến sẽ được mở từ 14h00 Thứ Năm, 12 tháng 01, 2023, và kết thúc vào lúc 07h00 Thứ Năm, 23 tháng 02, 2023.",
-  fees: "* Phí cơ bản cho 1 nội dung đầu tiên: 100.000₫ (Vietnamese Đồng).* Từ 2 nội dung trở lên: 30.000₫/nội dung* Phí cho 5 nội dung : 200.000₫ (Vietnamese Đồng).* Phí cho 10 nội dung : 300.000₫ (Vietnamese Đồng).* Phí cho 14 nội dung: 350.000₫ (Vietnamese Đồng).Đăng ký tại bàn ở giải đấu sẽ được chấp nhận với mức lệ phí nhân đôi so với đăng ký trên web.* Sau khi thanh toán lệ phí thi, đơn đăng ký của bạn sẽ được chấp nhận trong 24h Bạn có thể thanh toán lệ phí thi bằng cách chuyển khoản đến tài khoản dưới đây:Số tài khoản: (MB Bank – Ngân hàng Quân Đội) 0987896610Tên Tài Khoản: NGUYEN DUC VINH Sau khi chuyển khoản vui lòng gửi ảnh chứng minh giao dịch thành công cùng với tên của bạn hoặc ID WCA của bạn tới email: vncubeassociation@gmail.com Sau khi đăng ký và nộp lệ phí thi. Đăng ký của bạn sẽ được xác nhận trong vòng 24 giờ.Nếu đăng ký của bạn không được xác nhận trong vòng 24 giờ xin vui lòng liên hệ với chúng tôi. Nếu bạn không thể trả phí theo phương pháp trên, vui lòng liên hệ với chúng tôi. Lưu ý: tên đăng ký phải viết tiếng Việt có dấu, đúng thứ tự họ – tên đệm – tên, nếu không sẽ không được chấp nhận. Thí sinh có thể liên hệ ban tổ chức để thay đổi bộ môn thi, hạn cuối là ngày 25-02-2023. Tất cả thí sinh trong danh sách chờ sẽ bị loại bỏ và hoàn tiền (nếu đã đóng) sau ngày 22-02-2023. Tất cả lệ phí thi PHẢI ĐƯỢC chuyển khoản trước Sau khi đăng ký và chuyển khoản lệ phí, thời gian xác nhận đăng ký là 2-3 ngày làm việc. Nếu có thắc mắc, các bạn có thể email cho BTC. Với những bạn đã có vé tham dự WCA của Zyo Rubik (nhà tài trợ giải đấu), vui lòng mang theo vé và sẽ nhận lại tiền tại bàn check in thi đấu. Chi tiết liên hệ: ZyoRubik"
-}];
-
+export const ContentProps = [
+  {
+    id: 1,
+    title: "Giải Rubik WCA “Hanoi Super Brain Zyo 2023",
+    time: "Giải đấu sẽ được diễn ra trong 2 ngày: Ngày 25/02/2023: Bắt đầu thi đấu từ: 08h00 sáng – Kết thúc lúc 18h00 chiều. Ngày 26/02/2023: Bắt đầu thi đấu từ: 08h00 sáng – Kết thúc lúc 18h00 chiều.",
+    location:
+      "Giải đấu được tổ chức tại : The Sporthall, Newton Grammar School – Trường Quốc tế THCS – THPT Newton Địa chỉ: 136 Hồ Tùng Mậu, Bắc Từ Liêm District, Hà Nội. Click để xem trên Google Map",
+    signin:
+      "Đơn đăng ký trực tuyến sẽ được mở từ 14h00 Thứ Năm, 12 tháng 01, 2023, và kết thúc vào lúc 07h00 Thứ Năm, 23 tháng 02, 2023.",
+    req: "Đơn đăng ký trực tuyến sẽ được mở từ 14h00 Thứ Năm, 12 tháng 01, 2023, và kết thúc vào lúc 07h00 Thứ Năm, 23 tháng 02, 2023.",
+    fees: "* Phí cơ bản cho 1 nội dung đầu tiên: 100.000₫ (Vietnamese Đồng).* Từ 2 nội dung trở lên: 30.000₫/nội dung* Phí cho 5 nội dung : 200.000₫ (Vietnamese Đồng).* Phí cho 10 nội dung : 300.000₫ (Vietnamese Đồng).* Phí cho 14 nội dung: 350.000₫ (Vietnamese Đồng).Đăng ký tại bàn ở giải đấu sẽ được chấp nhận với mức lệ phí nhân đôi so với đăng ký trên web.* Sau khi thanh toán lệ phí thi, đơn đăng ký của bạn sẽ được chấp nhận trong 24h Bạn có thể thanh toán lệ phí thi bằng cách chuyển khoản đến tài khoản dưới đây:Số tài khoản: (MB Bank – Ngân hàng Quân Đội) 0987896610Tên Tài Khoản: NGUYEN DUC VINH Sau khi chuyển khoản vui lòng gửi ảnh chứng minh giao dịch thành công cùng với tên của bạn hoặc ID WCA của bạn tới email: vncubeassociation@gmail.com Sau khi đăng ký và nộp lệ phí thi. Đăng ký của bạn sẽ được xác nhận trong vòng 24 giờ.Nếu đăng ký của bạn không được xác nhận trong vòng 24 giờ xin vui lòng liên hệ với chúng tôi. Nếu bạn không thể trả phí theo phương pháp trên, vui lòng liên hệ với chúng tôi. Lưu ý: tên đăng ký phải viết tiếng Việt có dấu, đúng thứ tự họ – tên đệm – tên, nếu không sẽ không được chấp nhận. Thí sinh có thể liên hệ ban tổ chức để thay đổi bộ môn thi, hạn cuối là ngày 25-02-2023. Tất cả thí sinh trong danh sách chờ sẽ bị loại bỏ và hoàn tiền (nếu đã đóng) sau ngày 22-02-2023. Tất cả lệ phí thi PHẢI ĐƯỢC chuyển khoản trước Sau khi đăng ký và chuyển khoản lệ phí, thời gian xác nhận đăng ký là 2-3 ngày làm việc. Nếu có thắc mắc, các bạn có thể email cho BTC. Với những bạn đã có vé tham dự WCA của Zyo Rubik (nhà tài trợ giải đấu), vui lòng mang theo vé và sẽ nhận lại tiền tại bàn check in thi đấu. Chi tiết liên hệ: ZyoRubik",
+  },
+  {
+    id: 2,
+    title: "Giải Ho Chi Minh City Warm Up 2022",
+    time: "Giải đấu sẽ được diễn ra trong 1 ngày: Ngày 17/07/2022: Bắt đầu thi đấu từ: 07h30 sáng – Kết thúc lúc 16h00 chiều.",
+    location:
+      "Giải đấu được tổ chức tại : Cuộc thi tổ chức ở hội trường lớn tại trường Trung Cấp Ngề Hùng Vương Địa chỉ: 136 Hồ Tùng Mậu, Bắc Từ Liêm District, Hà Nội. Click để xem trên Google Map",
+    signin:
+      "Đăng ký trực tuyến đã mở vào Thứ Bảy, ngày 4 tháng 6 năm 2022 lúc 7:00 sáng GMT+7 và đóng vào Thứ Năm, ngày 14 tháng 7 năm 2022 lúc 7:00 sáng GMT+7.",
+    req: "Đơn đăng ký trực tuyến sẽ được mở từ 14h00 Thứ Năm, 12 tháng 01, 2023, và kết thúc vào lúc 07h00 Thứ Năm, 23 tháng 02, 2023.",
+    fees: "* Phí cơ bản cho 1 nội dung đầu tiên: 100.000₫ (Vietnamese Đồng).* Từ 2 nội dung trở lên: 30.000₫/nội dung* Phí cho 5 nội dung : 200.000₫ (Vietnamese Đồng).* Phí cho 10 nội dung : 300.000₫ (Vietnamese Đồng).* Phí cho 14 nội dung: 350.000₫ (Vietnamese Đồng).Đăng ký tại bàn ở giải đấu sẽ được chấp nhận với mức lệ phí nhân đôi so với đăng ký trên web.* Sau khi thanh toán lệ phí thi, đơn đăng ký của bạn sẽ được chấp nhận trong 24h Bạn có thể thanh toán lệ phí thi bằng cách chuyển khoản đến tài khoản dưới đây:Số tài khoản: (MB Bank – Ngân hàng Quân Đội) 0987896610Tên Tài Khoản: NGUYEN DUC VINH Sau khi chuyển khoản vui lòng gửi ảnh chứng minh giao dịch thành công cùng với tên của bạn hoặc ID WCA của bạn tới email: vncubeassociation@gmail.com Sau khi đăng ký và nộp lệ phí thi. Đăng ký của bạn sẽ được xác nhận trong vòng 24 giờ.Nếu đăng ký của bạn không được xác nhận trong vòng 24 giờ xin vui lòng liên hệ với chúng tôi. Nếu bạn không thể trả phí theo phương pháp trên, vui lòng liên hệ với chúng tôi. Lưu ý: tên đăng ký phải viết tiếng Việt có dấu, đúng thứ tự họ – tên đệm – tên, nếu không sẽ không được chấp nhận. Thí sinh có thể liên hệ ban tổ chức để thay đổi bộ môn thi, hạn cuối là ngày 25-02-2023. Tất cả thí sinh trong danh sách chờ sẽ bị loại bỏ và hoàn tiền (nếu đã đóng) sau ngày 22-02-2023. Tất cả lệ phí thi PHẢI ĐƯỢC chuyển khoản trước Sau khi đăng ký và chuyển khoản lệ phí, thời gian xác nhận đăng ký là 2-3 ngày làm việc. Nếu có thắc mắc, các bạn có thể email cho BTC. Với những bạn đã có vé tham dự WCA của Zyo Rubik (nhà tài trợ giải đấu), vui lòng mang theo vé và sẽ nhận lại tiền tại bàn check in thi đấu. Chi tiết liên hệ: ZyoRubik",
+  },
+];
 
 export const leagueData: LeagueItemProps[] = [
   {
@@ -30,17 +32,17 @@ export const leagueData: LeagueItemProps[] = [
     leagueImage: require("../../assets/image/Logo/item_logo.png"),
     date: "10-11-2023",
     icon: <Calendar size="20" color="#FF8A65" />,
-    params:{
+    params: {
       contentID: 1,
-    }
+    },
   },
   {
-    title: "Giải Rubik WCA “Hanoi Super Brain Zyo 2023”",
+    title: "Ho Chi Minh City Warm Up 2022”",
     leagueImage: require("../../assets/image/Logo/item_logo.png"),
-    date: "10-11-2023",
+    date: "17-11-2022",
     icon: <Calendar size="20" color="#FF8A65" />,
     params: {
-      contentID: 2
-    }
+      contentID: 2,
+    },
   },
 ];
