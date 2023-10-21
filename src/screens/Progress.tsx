bd98dfb680d5016a255d5e81ed331269902cfac3
Lokey2411 <hahaiviet24112003@gmail.com>
2023-10-21 23:17:04 +0700

    finish

--- src/screens/Progress.tsx
+++ src/screens/Progress.tsx
@@ -8,31 +8,29 @@ import NonInteractiveDropdown from "../components/NonInteractiveDropdown";
 
 const Progress = () => {
 	return (
-		<ScrollView>
-			<Box>
-				<Header1 title="Tiến độ học tập" />
-				<NonInteractiveDropdown
-					title="Rubik 3x3x3"
-					content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
-				/>
-				<NonInteractiveDropdown
-					title="Rubik 2x2x2"
-					content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
-				/>
-				<NonInteractiveDropdown
-					title="Rubik 4x4x4 trở lên"
-					content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
-				/>
-				<NonInteractiveDropdown
-					title="Rubik tam giác"
-					content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
-				/>
-				<NonInteractiveDropdown
-					title="Rubik gương"
-					content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
-				/>
-			</Box>
-		</ScrollView>
+		<Box>
+			<Header1 title="Tiến độ học tập" />
+			<NonInteractiveDropdown
+				title="Rubik 3x3x3"
+				content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
+			/>
+			<NonInteractiveDropdown
+				title="Rubik 2x2x2"
+				content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
+			/>
+			<NonInteractiveDropdown
+				title="Rubik 4x4x4 trở lên"
+				content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
+			/>
+			<NonInteractiveDropdown
+				title="Rubik tam giác"
+				content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
+			/>
+			<NonInteractiveDropdown
+				title="Rubik gương"
+				content={["Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu", "Hướng dẫn giải rubik cho người mới bắt đầu"]}
+			/>
+		</Box>
 	);
 };
 
